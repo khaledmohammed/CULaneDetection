2e4c2042e034814e617ab59dc75beb762689fae3
khaledmohammed <khaled.mohammed@gmail.com>
2019-05-21 00:26:43 -0700

    Moving the Model to a new file
    Refactoring the Model code to Model.py. The model is created in CreateModule() function.
    This will allow us to experiment with different model configurations.

---
+++ Model.py
@@ -0,0 +1,94 @@
+
+from keras.models import Sequential
+from keras.layers import Activation, Dropout, UpSampling2D, ZeroPadding2D
+from keras.layers import Conv2DTranspose, Conv2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+
+
+def CreateModel(input_shape):
+    batch_size = 16
+    pool_size = (2, 2)
+
+    ### Here is the actual neural network ###
+    model = Sequential()
+    # Normalizes incoming inputs. First layer needs the input shape to work
+    model.add(BatchNormalization(input_shape=input_shape))
+
+    # Below layers were re-named for easier reading of model summary; this not necessary
+    # Conv Layer 1
+    model.add(Conv2D(8, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv1'))
+
+    # Conv Layer 2
+    model.add(Conv2D(16, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv2'))
+
+    # Pooling 1
+    model.add(MaxPooling2D(pool_size=pool_size))
+
+    # Conv Layer 3
+    model.add(Conv2D(16, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv3'))
+    model.add(Dropout(0.2))
+
+    # Conv Layer 4
+    model.add(Conv2D(32, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv4'))
+    model.add(Dropout(0.2))
+
+    # Conv Layer 5
+    model.add(Conv2D(32, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv5'))
+    model.add(Dropout(0.2))
+
+    # Pooling 2
+    model.add(MaxPooling2D(pool_size=pool_size))
+
+    # Conv Layer 6
+    model.add(Conv2D(64, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv6'))
+    model.add(Dropout(0.2))
+
+    # Conv Layer 7
+    model.add(Conv2D(64, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv7'))
+    model.add(Dropout(0.2))
+
+    # Pooling 3
+    model.add(MaxPooling2D(pool_size=pool_size))
+
+    # Upsample 1
+    model.add(UpSampling2D(size=pool_size))
+
+    model.add(ZeroPadding2D(padding=((0,1),(0,0))))
+
+    # Deconv 1
+    model.add(Conv2DTranspose(64, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv1'))
+    model.add(Dropout(0.2))
+
+    # Deconv 2
+    model.add(Conv2DTranspose(64, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv2'))
+    model.add(Dropout(0.2))
+
+    # Upsample 2
+    model.add(UpSampling2D(size=pool_size))
+
+    # Deconv 3
+    model.add(Conv2DTranspose(32, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv3'))
+    model.add(Dropout(0.2))
+
+    # Deconv 4
+    model.add(Conv2DTranspose(32, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv4'))
+    model.add(Dropout(0.2))
+
+    # Deconv 5
+    model.add(Conv2DTranspose(16, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv5'))
+    model.add(Dropout(0.2))
+
+    # Upsample 3
+    model.add(UpSampling2D(size=pool_size))
+
+    model.add(ZeroPadding2D(padding=((2,0),(0,0))))
+
+    # Deconv 6
+    model.add(Conv2DTranspose(16, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv6'))
+
+    # Final layer - only including one channel so 3 filter
+    model.add(Conv2DTranspose(3, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Final'))
+
+    ### End of network ###
+
+    return model
--- TrainDataSet.py
+++ TrainDataSet.py
@@ -6,14 +6,11 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 # Import necessary items from Keras
-from keras.models import Sequential
-from keras.layers import Activation, Dropout, UpSampling2D, ZeroPadding2D
-from keras.layers import Conv2DTranspose, Conv2D, MaxPooling2D
-from keras.layers.normalization import BatchNormalization
 from keras.preprocessing.image import ImageDataGenerator
 from keras import regularizers
 import keras as K
 
+from Model import CreateModel
 
 matplotlib.use("Agg")
 
@@ -40,92 +37,12 @@ X_train, X_val, y_train, y_val = train_test_split(train_images, labels, test_siz
 
 # Batch size, epochs and pool size below are all paramaters to fiddle with for optimization
 batch_size = 16
-epochs = 100
+epochs = 10
 pool_size = (2, 2)
 input_shape = X_train.shape[1:]
 
-### Here is the actual neural network ###
-model = Sequential()
-# Normalizes incoming inputs. First layer needs the input shape to work
-model.add(BatchNormalization(input_shape=input_shape))
-
-# Below layers were re-named for easier reading of model summary; this not necessary
-# Conv Layer 1
-model.add(Conv2D(8, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv1'))
-
-# Conv Layer 2
-model.add(Conv2D(16, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv2'))
-
-# Pooling 1
-model.add(MaxPooling2D(pool_size=pool_size))
-
-# Conv Layer 3
-model.add(Conv2D(16, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv3'))
-model.add(Dropout(0.2))
-
-# Conv Layer 4
-model.add(Conv2D(32, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv4'))
-model.add(Dropout(0.2))
-
-# Conv Layer 5
-model.add(Conv2D(32, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv5'))
-model.add(Dropout(0.2))
-
-# Pooling 2
-model.add(MaxPooling2D(pool_size=pool_size))
-
-# Conv Layer 6
-model.add(Conv2D(64, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv6'))
-model.add(Dropout(0.2))
-
-# Conv Layer 7
-model.add(Conv2D(64, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv7'))
-model.add(Dropout(0.2))
-
-# Pooling 3
-model.add(MaxPooling2D(pool_size=pool_size))
-
-# Upsample 1
-model.add(UpSampling2D(size=pool_size))
-
-model.add(ZeroPadding2D(padding=((0,1),(0,0))))
-
-# Deconv 1
-model.add(Conv2DTranspose(64, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv1'))
-model.add(Dropout(0.2))
-
-# Deconv 2
-model.add(Conv2DTranspose(64, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv2'))
-model.add(Dropout(0.2))
-
-# Upsample 2
-model.add(UpSampling2D(size=pool_size))
-
-# Deconv 3
-model.add(Conv2DTranspose(32, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv3'))
-model.add(Dropout(0.2))
-
-# Deconv 4
-model.add(Conv2DTranspose(32, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv4'))
-model.add(Dropout(0.2))
-
-# Deconv 5
-model.add(Conv2DTranspose(16, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv5'))
-model.add(Dropout(0.2))
-
-# Upsample 3
-model.add(UpSampling2D(size=pool_size))
-
-model.add(ZeroPadding2D(padding=((2,0),(0,0))))
-
-# Deconv 6
-model.add(Conv2DTranspose(16, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv6'))
-
-# Final layer - only including one channel so 3 filter
-model.add(Conv2DTranspose(3, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Final'))
-
-### End of network ###
-
+model = CreateModel(input_shape)
+model.summary()
 
 # Using a generator to help the model use less data
 # Channel shifts help with shadows slightly
@@ -174,5 +91,5 @@ model.compile(optimizer='Adam', loss='mean_squared_error')
 model.save('Model.h5')
 
 # Show summary of model
-#model.summary()
+
 
