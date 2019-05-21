0bd4968216e1f053eb3cc46e57d2e35d4d772366
khaledmohammed <khaled.mohammed@gmail.com>
2019-05-21 13:11:45 -0700

    Implemented Custom Generator
    This is allowing us to train on huge dataset. I'm currently training the model on 16000 images with 10 epochs.

--- DataSet.py
+++ DataSet.py
@@ -1,5 +1,10 @@
-
+import sys
 import pickle
+import cv2
+import os, fnmatch
+import glob
+import sys
+import gc
 import numpy as np
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
@@ -24,3 +29,45 @@ def GetPickleDataSet():
     X_train, X_val, y_train, y_val = train_test_split(train_images, labels, test_size=0.1)
     
     return (X_train, y_train, X_val, y_val)
+
+
+def TrainDataGenerator(batch_size, mode):
+    X_top_folder = 'D:/cs230-project/CULaneOriginalImage'
+    Y_top_folder = 'D:/cs230-project/CULaneLabels/'
+    
+    count = 1
+    skipCount = 0
+    X_train = []
+    Y_train = []
+    for subDir in os.listdir(X_top_folder):
+        X_path = os.path.join(X_top_folder, subDir)
+        Y_path = os.path.join(Y_top_folder, subDir)
+        for subDir2 in os.listdir(X_path):
+            X_path2 = os.path.join(X_path, subDir2)
+            Y_path2 = os.path.join(Y_path, subDir2)
+            for file in os.listdir(X_path2):
+                if fnmatch.fnmatch(file, "*.jpg"):
+                    X_img_file = os.path.join(X_path2, file)
+                    Y_img_file = os.path.join(Y_path2, file)
+                    Y_img_file = os.path.splitext(Y_img_file)[0] + '.png'
+                    if (os.path.isfile(Y_img_file)):
+                        skipCount = skipCount + 1
+                        if mode=="dev" and skipCount % 100 == 0:
+                            continue
+                        imgArrY = cv2.imread(Y_img_file, cv2.IMREAD_COLOR)
+                        imgArrX = cv2.imread(X_img_file, cv2.IMREAD_COLOR)
+                        X_train.append(imgArrX)
+                        Y_train.append(imgArrY)
+                        if count % batch_size == 0:
+#                            print("New batch: ", X_path2, 'Img Cnt: ', count)
+                            yield((np.array(X_train), np.array(Y_train)))
+                            X_train = []
+                            Y_train = []                            
+                        count = count + 1
+                    
+    
+
+    
+d = TrainDataGenerator(16, 'dev')
+print(next(d)[0].shape)
+print(next(d)[0].shape)
--- TrainDataSet.py
+++ TrainDataSet.py
@@ -7,31 +7,32 @@ import matplotlib.pyplot as plt
 # Import necessary items from Keras
 from keras.preprocessing.image import ImageDataGenerator
 import keras as K
-from DataSet import GetPickleDataSet
+import DataSet
 from Model import CreateModel
 
 
 matplotlib.use("Agg")
 
-
-(X_train, y_train, X_val, y_val) = GetPickleDataSet()
+#(X_train, y_train, X_val, y_val) = DataSet.GetPickleDataSet()
 
 # Batch size, epochs and pool size below are all paramaters to fiddle with for optimization
 batch_size = 16
 epochs = 10
-input_shape = X_train.shape[1:]
+input_shape = (590, 1640, 3)
 
 model = CreateModel(input_shape)
 model.summary()
 
-# Using a generator to help the model use less data
-# Channel shifts help with shadows slightly
-#datagen = ImageDataGenerator(channel_shift_range=0.2)
-#datagen.fit(X_train)
 
 # Compiling and training the model
 model.compile(optimizer='Adam', loss='mean_squared_error', metrics=["mae", "acc"])
-history = model.fit(x=X_train, y=y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(X_val, y_val))
+#history = model.fit(x=X_train, y=y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(X_val, y_val))
+
+trainGen = DataSet.TrainDataGenerator(batch_size=batch_size, mode='train')
+devGen = DataSet.TrainDataGenerator(batch_size=batch_size, mode='dev')
+
+history = model.fit_generator(generator=trainGen, steps_per_epoch=1000, epochs=epochs,
+                              verbose=1, validation_data=devGen, validation_steps=100, workers=1) 
 
 # plot a graph 
 N = epochs
@@ -46,22 +47,6 @@ plt.xlabel("Epoch #")
 plt.ylabel("Loss/Accuracy")
 plt.legend(loc="lower left")
 plt.savefig("plot.png")
-
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.savefig('accuracy.png')
-
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.savefig('loss.png')
 
 # Freeze layers since training is done
 model.trainable = False
