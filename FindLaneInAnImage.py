94f259b76a92b5e998cfbed8471b26563e622bb3
Khaled Mohammed <khaled.mohammed@gmail.com>
2019-06-04 00:47:15 +0000

    Looks good!

--- FindLaneInAnImage.py
+++ FindLaneInAnImage.py
@@ -2,7 +2,7 @@ import numpy as np
 from keras.models import load_model
 import cv2
 import matplotlib.pyplot as plt
-import os, sys, datetime
+import os, sys, datetime, platform
 import DataSet
 from PIL import Image
 
@@ -11,8 +11,8 @@ from PIL import Image
 def ApplyLaneDetection(Y_hat, XtestImg):
     for i in range(Y_hat.shape[0]):
         for j in range(Y_hat.shape[1]):
-            rvalue = int(Y_hat[i][j][0])
-            if (rvalue>=1):
+            rvalue = Y_hat[i][j]
+            if (rvalue>0.5):
                 XtestImg[i][j][1] = 255
     
 def SaveInFolder(id, XtestImg, Y, out_dir):
@@ -30,11 +30,15 @@ def SaveInFolder(id, XtestImg, Y, out_dir):
 currentDir = os.path.dirname(os.path.realpath(__file__))
 model = load_model(os.path.join(currentDir, 'Model.h5'))
 
-mydir = os.path.join(os.getcwd(), datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+resultdir = os.getcwd()
+if platform.system() == 'Linux':
+    resultdir = '/var/www/html/results'
+
+mydir = os.path.join(resultdir, datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
 os.makedirs(mydir)
 
 
-ds = DataSet.TrainDataGenerator(DataSet.dev_set_count, 'dev')
+ds = DataSet.TrainDataGenerator(DataSet.dev_set_count, 'dev', 0.2)
 #ds = DataSet.TrainDataGenerator(10, 'dev')
 
 devSet = next(ds)
@@ -51,7 +55,8 @@ out = open(htmlfile, 'w')
 out.write('<html><body><table>')
 for ii in range(XList.shape[0]):
     tempX = np.copy(XList[ii])
-    ApplyLaneDetection(Y_hat[ii], XList[ii])
+    print(ii, np.count_nonzero(Y_hat[ii]))
+    ApplyLaneDetection(Y_hat[ii], XList[ii])    
     ApplyLaneDetection(YList[ii], tempX)
     SaveInFolder(ii, XList[ii], tempX, mydir)
     out.write('<tr><td><img src=\'' + str(ii) + '.jpg\'' + hw + '></td><td><img src=\'' + str(ii)+'_Y.jpg\'' + hw + '></td></tr>')
--- TrainDataSet.py
+++ TrainDataSet.py
@@ -18,9 +18,9 @@ matplotlib.use("Agg")
 
 # Batch size, epochs and pool size below are all paramaters to fiddle with for optimization
 image_resizing_factor = 0.2
-batch_size = 128
-epochs = 150
-input_shape = (int(590 * image_resizing_factor), int(1640 * image_resizing_factor), 3)
+batch_size = 32
+epochs = 75
+input_shape = (590 * image_resizing_factor, 1640 * image_resizing_factor, 3)
 
 model = CreateModel(input_shape)
 
@@ -33,39 +33,20 @@ def recall(y_true, y_pred):
     return recall
 
 def f1(y_true, y_pred):
-    def recall(y_true, y_pred):
-        """Recall metric.
+    true_positives = KB.sum(KB.round(KB.clip(y_true * y_pred, 0, 1)))
+    possible_positives = KB.sum(KB.round(KB.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + KB.epsilon())
+#    return recall
+    predicted_positives = KB.sum(KB.clip(y_pred, 0, 1))
+    precision = true_positives / (predicted_positives + KB.epsilon())
 
-        Only computes a batch-wise average of recall.
-
-        Computes the recall, a metric for multi-label classification of
-        how many relevant items are selected.
-        """
-        true_positives = KB.sum(KB.round(KB.clip(y_true * y_pred, 0, 1)))
-        possible_positives = KB.sum(KB.round(KB.clip(y_true, 0, 1)))
-        recall = true_positives / (possible_positives + KB.epsilon())
-        return recall
-
-    def precision(y_true, y_pred):
-        """Precision metric.
-
-        Only computes a batch-wise average of precision.
-
-        Computes the precision, a metric for multi-label classification of
-        how many selected items are relevant.
-        """
-        true_positives = KB.sum(KB.round(KB.clip(y_true * y_pred, 0, 1)))
-        predicted_positives = KB.sum(KB.round(KB.clip(y_pred, 0, 1)))
-        precision = true_positives / (predicted_positives + KB.epsilon())
-        return precision
-    precision = precision(y_true, y_pred)
-    recall = recall(y_true, y_pred)
+#    return precision
     return 2*((precision*recall)/(precision+recall+KB.epsilon()))
 
 
 # Compiling and training the model
-optimizer = K.optimizers.Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
-model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=[f1, recall, 'acc'])
+optimizer = K.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+model.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['acc', f1])
 
 trainGen = DataSet.TrainDataGenerator(batch_size=batch_size, mode='train', image_resizing_factor=image_resizing_factor)
 devGen = DataSet.TrainDataGenerator(batch_size=batch_size, mode='dev', image_resizing_factor=image_resizing_factor)
@@ -75,27 +56,30 @@ steps_per_epoch = DataSet.train_set_count // batch_size
 history = model.fit_generator(generator=trainGen, steps_per_epoch=steps_per_epoch, epochs=epochs,
                               verbose=1, validation_data=devGen, validation_steps=validation_steps) 
 
+
+# Freeze layers since training is done
+model.trainable = False
+model.compile(optimizer=optimizer, loss='mean_squared_error')
+
+# Save model architecture and weights
+model.save('Model.h5')
+
 # plot a graph 
 N = epochs
 plt.style.use("ggplot")
 plt.figure()
 plt.plot(np.arange(0, N), history.history["loss"], label="train_loss")
 plt.plot(np.arange(0, N), history.history["val_loss"], label="val_loss")
+#plt.plot(np.arange(0, N), history.history["recall"], label="recall")
+#plt.plot(np.arange(0, N), history.history["f1"], label="f1")
 plt.plot(np.arange(0, N), history.history["acc"], label="train_acc")
 plt.plot(np.arange(0, N), history.history["val_acc"], label="val_acc")
+#plt.plot(np.arange(0, N), history.history["val_f1"], label="val_f1")
+#plt.plot(np.arange(0, N), history.history["val_recall"], label="val_recall")
 plt.title("Training Loss and Accuracy on Dataset")
 plt.xlabel("Epoch #")
 plt.ylabel("Loss/Accuracy")
 plt.legend(loc="lower left")
 plt.savefig("plot.png")
 
-# Freeze layers since training is done
-model.trainable = False
-model.compile(optimizer=optimizer, loss='binary_crossentropy')
-
-# Save model architecture and weights
-model.save('Model.h5')
-
-# Show summary of model
-model.summary()
 
